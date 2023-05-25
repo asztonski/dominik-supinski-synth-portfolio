b33a04ec831489f97f75664456ca8fe66fd7108a
Dominik Supinski <dominik.supinski@getsavvy.com>
2023-05-25 18:48:44 +0100

    Helper texts + validation updates in progress

--- src/components/UI/Form/Form.jsx
+++ src/components/UI/Form/Form.jsx
@@ -12,6 +12,8 @@ import {
 import { useContext } from "react";
 import { AppContext } from "@/api/AppContext";
 import FormHelperText from "@mui/material";
+import CheckIcon from "@/assets/svg/check";
+import { Keyframes } from "@emotion/react";
 
 const Form = () => {
   const [inputValue, setInputValue] = useState({
@@ -22,6 +24,9 @@ const Form = () => {
 
   const { setIsFocused } = useContext(AppContext);
 
+  const [isNameValid, setIsNameValid] = useState(null);
+  const [isMailValid, setIsMailValid] = useState(null);
+  const [isMessageValid, setIsMessageValid] = useState(null);
   const [isFormValid, setIsFormValid] = useState(false);
 
   const handleInputChange = (e) => {
@@ -51,9 +56,21 @@ const Form = () => {
     setIsFormValid(isAllInputsValid);
   };
 
-  useEffect(() => {
-    validateForm();
-  }, [inputValue]);
+  // useEffect(() => {
+  //   validateName(inputValue.name)
+  //     ? setIsNameValid(true)
+  //     : setIsNameValid(false);
+  //   validateName(inputValue.email)
+  //     ? setIsMailValid(true)
+  //     : setIsMailValid(false);
+  //   validateName(inputValue.message)
+  //     ? setIsMessageValid(true)
+  //     : setIsMessageValid(false);
+  // }, []);
+
+  // useEffect(() => {
+  //   validateForm();
+  // }, [inputValue]);
 
   const handleSubmit = (e) => {
     const { name, email, message } = inputValue;
@@ -75,6 +92,49 @@ const Form = () => {
   //   } else return false;
   // };
 
+  const errorHandler = (input) => {
+    if (input.name === "name") {
+      if (validateName(inputValue.name)) {
+        setIsNameValid(true);
+      } else setIsNameValid(false);
+    }
+
+    if (input.name === "email") {
+      if (validateName(inputValue.email)) {
+        setIsMailValid(true);
+      } else setIsMailValid(false);
+    }
+
+    if (input.name === "message") {
+      if (validateName(inputValue.message)) {
+        setIsMessageValid(true);
+      } else setIsMessageValid(false);
+    }
+  };
+
+  const errorTextHandler = (input) => {
+    const text = "Please provide correct ";
+    const result = text + input.name;
+
+    if (input.name === "name" && isNameValid === false) {
+      return result;
+    }
+
+    if (input.name === "email" && isMailValid === false) {
+      return result;
+    }
+
+    if (input.name === "message" && isMessageValid === false) {
+      return result + " that has minimum 100 characters";
+    }
+
+    return false;
+  };
+
+  useEffect(() => {
+    console.log(isNameValid);
+  }, [isNameValid]);
+
   return (
     <FormContainer>
       {formItems.map((input) => (
@@ -90,8 +150,8 @@ const Form = () => {
           multiline={input.isMultiline}
           rows={input.rows}
           onClick={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
-          // helperText={errorHandler(input)}
+          onBlur={() => (setIsFocused(false), errorHandler(input))}
+          helperText={errorTextHandler(input)}
           sx={{
             border: `1px solid ${theme.colors.squares}`,
           }}
@@ -100,6 +160,13 @@ const Form = () => {
               color: `${theme.colors.primary}`,
               fontStyle: "italic",
             },
+            // endAdornment: (
+            //   <CheckIcon
+            //     color={theme.colors.hover}
+            //     height="20px"
+            //     width="20px"
+            //   />
+            // ),
           }}
           InputLabelProps={{
             style: {
@@ -122,16 +189,28 @@ export default Form;
 const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
-  gap: 1.5rem;
+  gap: 2.5rem;
   width: 100%;
   div {
+    position: relative;
     background: transparent !important;
+    /* padding-top: 10px; */
     label {
       text-transform: uppercase;
     }
     input {
       text-transform: none;
     }
+    p {
+      position: absolute;
+      bottom: -2rem;
+      color: ${theme.colors.extra};
+    }
+    /* svg {
+      position: absolute;
+      right: 3%;
+      top: 0;
+    } */
   }
   button {
     align-self: flex-start;
