5dbd79aede265b47cac5ac5f292a27dff623aeb6
asztonski <asztonski@gmail.com>
2025-11-10 15:00:19 -0500

    Fix navUpArrowLinkHandler: update return value for stage 2 to link to home section

--- src/components/UI/NavBar/NavBar.jsx
+++ src/components/UI/NavBar/NavBar.jsx
@@ -37,7 +37,7 @@ const NavBar = ({ style }) => {
 
   const navUpArrowLinkHandler = () => {
     if (stage === 2) {
-      return "";
+      return "#home";
     }
     if (stage === 3) {
       return "#about";
